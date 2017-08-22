b395216cb0112ea44c840369b3c7419bfeddb47e
Paul Schulz <paul@mawsonlaes.org>
2017-08-22 09:31:39 +0930

    Added volume setting to start.sh script.
    Volume is not preserved between reboots. The start,sh script
    needs to set the volume on the PWM output to its loudest possible
    setting on boot. 90% chosen as at 100% audio distorts a little.
    
    Also, made all setup commands in start.sh sequential, rather than
    going into backbround, as this caused a large system load which
    caused chopping in audio playback.

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ${0%/*}
+cd ${0%/*} #
 
 /usr/bin/gpio mode 0 in
 /usr/bin/gpio mode 1 in
@@ -10,9 +10,25 @@ cd ${0%/*}
 /usr/bin/gpio mode 1 tri
 /usr/bin/gpio mode 2 tri
 
-cd $HOME/git/cosmic-array/bin
+sudo amixer -c 0 set PCM playback 90% unmute >/dev/null 2>&1
 
-/usr/bin/espeak "Cosmic Array Sensor Online. Node name `hostname`" >/dev/null 2>&1 &
+#######################################################################
+# Selftest
+/usr/bin/espeak "Channel 0" >/dev/null 2>&1
+aplay audio/channel0.wav    >/dev/null 2>&1
+
+/usr/bin/espeak "Channel 1" >/dev/null 2>&1
+aplay audio/channel1.wav    >/dev/null 2>&1
+
+/usr/bin/espeak "Channel 2" >/dev/null 2>&1
+aplay audio/channel2.wav    >/dev/null 2>&1
+
+/usr/bin/espeak "Channel 3" >/dev/null 2>&1
+aplay audio/channel3.wav    >/dev/null 2>&1
+#######################################################################
+
+/usr/bin/espeak "Cosmic Array Sensor Online. Node name `hostname`" \
+  >/dev/null 2>&1
 
 $HOME/git/cosmic-array/bin/shoreman >/dev/null 2>&1 
 
