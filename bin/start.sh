bcba9ffd8059c9e2ac300c200beca77cdf6759d7
Paul Schulz <paul@mawsonlakes.org>
2017-09-02 00:34:26 +0930

    Get start.sh script to select audio to play, based on detector name.

--- bin/start.sh
+++ bin/start.sh
@@ -15,6 +15,18 @@ sudo amixer -c 0 set PCM playback 97% unmute >/dev/null 2>&1
 #######################################################################
 /usr/bin/espeak "Cosmic Array Sensor starting" >/dev/null 2>&1
 
+if [ -d  audio/`hostname` ]; then
+        /usr/bin/espeak "Host based audio found." >/dev/null 2>&1
+	rm audio/channel0.wav; ln -s `hostname`/channel0.wav audio/channel0.wav
+	rm audio/channel1.wav; ln -s `hostname`/channel1.wav audio/channel1.wav
+	rm audio/channel2.wav; ln -s `hostname`/channel2.wav audio/channel2.wav
+else
+        /usr/bin/espeak "Default audio being used." >/dev/null 2>&1
+	rm audio/channel0.wav; ln -s default/channel0.wav audio/channel0.wav
+	rm audio/channel1.wav; ln -s default/channel1.wav audio/channel1.wav
+	rm audio/channel2.wav; ln -s default/channel2.wav audio/channel2.wav
+fi
+
 /usr/bin/espeak "Channel 0" >/dev/null 2>&1
 aplay audio/channel0.wav    >/dev/null 2>&1
 
