9ba362c99f0a448cb4cbc2e815d48700fb978443
Paul Schulz <paul@mawsonlakes.org>
2018-09-01 07:30:53 +0000

    Moved channel 2 interupt to pin3 (wiringPi)

--- src/play/cosmic-array-play.c
+++ src/play/cosmic-array-play.c
@@ -14,9 +14,9 @@ void error(char *msg) {
 }
 
 /* interrupt functions */
-void my_interrupt(int i) {
-  system("aplay audio/other.wav > /dev/null 2>&1 &");
-}
+// void my_interrupt(int i) {
+//   system("aplay audio/other.wav > /dev/null 2>&1 &");
+// }
 
 void my_interrupt_0 (void) {
   system("aplay audio/channel0.wav > /dev/null 2>&1 &");
@@ -34,10 +34,10 @@ void my_interrupt_3 (void) {
   system("aplay audio/channel3.wav > /dev/null 2>&1 &");
 }
 
-void my_interrupt_4 (void) { my_interrupt(4); }
-void my_interrupt_5 (void) { my_interrupt(5); }
-void my_interrupt_6 (void) { my_interrupt(6); }
-void my_interrupt_7 (void) { my_interrupt(7); }
+// void my_interrupt_4 (void) { my_interrupt(4); }
+// void my_interrupt_5 (void) { my_interrupt(5); }
+// void my_interrupt_6 (void) { my_interrupt(6); }
+// void my_interrupt_7 (void) { my_interrupt(7); }
 
 int main(int argc, char **argv) {
   wiringPiSetup();
@@ -48,15 +48,19 @@ int main(int argc, char **argv) {
     exit(0);
   }
 
-  wiringPiISR (0, INT_EDGE_RISING, &my_interrupt_0) ;
-  wiringPiISR (1, INT_EDGE_RISING, &my_interrupt_1) ;
-  wiringPiISR (2, INT_EDGE_RISING, &my_interrupt_2) ;
-  wiringPiISR (3, INT_EDGE_RISING, &my_interrupt_3) ;
-  wiringPiISR (4, INT_EDGE_RISING, &my_interrupt_4) ;
-  wiringPiISR (5, INT_EDGE_RISING, &my_interrupt_5) ;
-  wiringPiISR (6, INT_EDGE_RISING, &my_interrupt_6) ;
-  wiringPiISR (7, INT_EDGE_RISING, &my_interrupt_7) ;
+  wiringPiISR(0, INT_EDGE_RISING, &my_interrupt_0);
+  // The second interupt line  is used by the I2S interface.
+  //  wiringPiISR(1, INT_EDGE_RISING, &my_interrupt_1);
+  wiringPiISR(2, INT_EDGE_RISING, &my_interrupt_2);
+  wiringPiISR(3, INT_EDGE_RISING, &my_interrupt_1);
 
+  // Only set interupt routines that are required.
+  // wiringPiISR(4, INT_EDGE_RISING, &my_interrupt_4);
+  // wiringPiISR(5, INT_EDGE_RISING, &my_interrupt_5);
+  // wiringPiISR(6, INT_EDGE_RISING, &my_interrupt_6);
+  // wiringPiISR(7, INT_EDGE_RISING, &my_interrupt_7);
+
+  // Spin, with interupts running.
   for(;;){
     delay(100);
   }
--- src/send/cosmic-array-send.c
+++ src/send/cosmic-array-send.c
@@ -91,13 +91,13 @@ void my_interrupt(int i){
 }
 
 void my_interrupt_0 (void) { my_interrupt(0); }
-void my_interrupt_1 (void) { my_interrupt(1); }
+// void my_interrupt_1 (void) { my_interrupt(1); }
 void my_interrupt_2 (void) { my_interrupt(2); }
-void my_interrupt_3 (void) { my_interrupt(3); }
-void my_interrupt_4 (void) { my_interrupt(4); }
-void my_interrupt_5 (void) { my_interrupt(5); }
-void my_interrupt_6 (void) { my_interrupt(6); }
-void my_interrupt_7 (void) { my_interrupt(7); }
+void my_interrupt_3 (void) { my_interrupt(1); }
+// void my_interrupt_4 (void) { my_interrupt(4); }
+// void my_interrupt_5 (void) { my_interrupt(5); }
+// void my_interrupt_6 (void) { my_interrupt(6); }
+// void my_interrupt_7 (void) { my_interrupt(7); }
 
 int main(int argc, char **argv) {
   int pin;
@@ -138,14 +138,14 @@ int main(int argc, char **argv) {
 	(char *)&serveraddr.sin_addr.s_addr, server->h_length);
   serveraddr.sin_port = htons(portno);
   
-  wiringPiISR (0, INT_EDGE_RISING, &my_interrupt_0) ;
-  wiringPiISR (1, INT_EDGE_RISING, &my_interrupt_1) ;
-  wiringPiISR (2, INT_EDGE_RISING, &my_interrupt_2) ;
-  wiringPiISR (3, INT_EDGE_RISING, &my_interrupt_3) ;
-  wiringPiISR (4, INT_EDGE_RISING, &my_interrupt_4) ;
-  wiringPiISR (5, INT_EDGE_RISING, &my_interrupt_5) ;
-  wiringPiISR (6, INT_EDGE_RISING, &my_interrupt_6) ;
-  wiringPiISR (7, INT_EDGE_RISING, &my_interrupt_7) ;
+  wiringPiISR(0, INT_EDGE_RISING, &my_interrupt_0);
+  // wiringPiISR(1, INT_EDGE_RISING, &my_interrupt_1);
+  wiringPiISR(2, INT_EDGE_RISING, &my_interrupt_2);
+  wiringPiISR(3, INT_EDGE_RISING, &my_interrupt_3);
+  // wiringPiISR(4, INT_EDGE_RISING, &my_interrupt_4);
+  // wiringPiISR(5, INT_EDGE_RISING, &my_interrupt_5);
+  // wiringPiISR(6, INT_EDGE_RISING, &my_interrupt_6);
+  // wiringPiISR(7, INT_EDGE_RISING, &my_interrupt_7);
 
   for(;;){
     delay(100);
